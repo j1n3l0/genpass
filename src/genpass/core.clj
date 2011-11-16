597dcd42dfe31cda280c02b30896140d4a12c58d
Nelo Onyiah <nelo.onyiah@gmail.com>
2011-11-16 19:48:55 +0000

    A basic random password generator

--- src/genpass/core.clj
+++ src/genpass/core.clj
@@ -1,1 +1,20 @@
 (ns genpass.core)
+
+(defn- chars-for
+  [start end]
+  (map char (range start end)))
+
+(def caps (chars-for 65 91))
+(def lows (chars-for 97 123))
+(def numbers (range 10))
+(def symbols (chars-for 33 47))
+
+(defn password
+  "Generate a random password - default length = 8"
+  ([] (password 8))
+  ([length]
+     (apply str
+            (loop [n length pass []]
+              (if (>= 0 n)
+                pass
+                (recur (dec n) (conj pass (rand-nth (concat caps lows numbers symbols)))))))))
