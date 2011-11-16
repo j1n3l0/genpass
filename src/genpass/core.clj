96d245d384ca336c70476c26c417acd9fcda612a
Nelo Onyiah <nelo.onyiah@gmail.com>
2011-11-16 19:37:18 +0000

    Initial commit

---
+++ src/genpass/core.clj
@@ -0,0 +1,1 @@
+(ns genpass.core)
---
+++ test/genpass/test/core.clj
@@ -0,0 +1,6 @@
+(ns genpass.test.core
+  (:use [genpass.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
